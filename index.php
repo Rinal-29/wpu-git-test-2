427a03227f4e4ab7459941724deaf9a0b234003f
rinal <afrinalhakim29@gmail.com>
2020-01-15 10:25:21 +0700

    menambah paragraf pada index.html

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste molestias, eveniet fugit sapiente facere minima dolore autem quia fugiat tempora sint perferendis iure molestiae a labore voluptates esse eos ad.</p>
 </body>
 
 </html>
